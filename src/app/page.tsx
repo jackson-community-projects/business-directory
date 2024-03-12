f216dfcd50dc8735da0fc8897e53a0b72c519cd5
John Weland <john.weland@gmail.com>
2024-03-12 15:58:55 -0500

    deploy for testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -100,7 +100,7 @@ const Page: NextPage = () => {
                   name="search"
                   id="search"
                   onChange={(event) => setBusinessQuery(event.target.value)}
-                  placeholder="Search for a business by name or category"
+                  placeholder="Search business name or category"
                   className="block h-12 w-full rounded-sm border-0 py-1.5 pl-12 pr-10 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-emerald-600 sm:text-sm sm:leading-6"
                 />
                 <div className="left-o absolute inset-y-0 inline-flex items-center py-1.5 pl-2">
