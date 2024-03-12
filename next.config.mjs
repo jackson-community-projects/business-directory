e34c0938a25ada3f5ef279a6339eff2ef455f62a
John Weland <john.weland@gmail.com>
2024-03-12 15:09:58 -0500

    deploy for testing

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: "/business-directory",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
